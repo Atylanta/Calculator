e370c4287472e838623e08eff67504f94406aca7
Atylanta <46928797+Atylanta@users.noreply.github.com>
2019-02-05 16:08:23 +0300

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -39,58 +39,58 @@ var changes = false;
 
 
 function clickPercent(){
-    //Описываем условия написания знака
-    if (arrayMemoryNumbers.length < 1 && !changes) { //если экран пуст
+    //Описываем условия написания знака \ describing the conditions of using the symbol
+    if (arrayMemoryNumbers.length < 1 && !changes) { //если экран пуст \ if the display is clear
         display.value = 0;
-    } else if (changes) { // Если мы ввели новое число
-        if (changes && arrayMemoryNumbers.length < 1) { //если на экране только 1 число
+    } else if (changes) { // Если мы ввели новое число \ if we write a new number
+        if (changes && arrayMemoryNumbers.length < 1) { //если на экране только 1 число \ if we write only 1 number
             display.value = 0;
-        } else { //если на экране больше 1 числа
+        } else { //если на экране больше 1 числа \ if there is more then 1 number
             display.value = arrayMemoryNumbers[arrayMemoryNumbers.length-1] / 100 * display.value;
         }
-    } else if (!changes && display.value == resultEquall) { //Если на экране - результат функции равно
+    } else if (!changes && display.value == resultEquall) { //Если на экране - результат функции равно \ if it's a result of Equall function on the display
         return display.value;
-    } else { //Если предыдущее значение - знак или это посчитанный результат
+    } else { //Если предыдущее значение - знак или это посчитанный результат \ if the last value - it's an operation symbol
         return display.value;
     }
 }
 PERCENT.addEventListener('click', clickPercent);
 
 function clickRoot(){
-    //Описываем условия написания знака
-    if (display.value === 0) { //Если экран полностью пуст
+    //Описываем условия написания знака \ describing the conditions of using the symbol
+    if (display.value === 0) { //Если экран полностью пуст \ if the display is clear
         display.value = 0;
-    } else if (changes) { // Если мы ввели новое число
+    } else if (changes) { // Если мы ввели новое число \ if we write a new number
         display.value = Math.sqrt(display.value);
-    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить кв.крень результата
+    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить кв.кoрень результата \ if we need to have the square root of the number
         display.value = Math.sqrt(display.value);
-    } else { //Если предыдущее значение - знак или это посчитанный результат
+    } else { //Если предыдущее значение - знак или это посчитанный результат \ if the last value - it's an operation symbol or a result of Equall function
         return display.value;
     }
 }
 ROOT.addEventListener('click', clickRoot);
 
 function clickPower(){
-    if (display.value === 0) { //Если экран полностью пуст
+    if (display.value === 0) { //Если экран полностью пуст \ if the display is clear
         display.value = 0;
-    } else if (changes) { // Если мы ввели новое число
+    } else if (changes) { // Если мы ввели новое число \ if we write a new number
         display.value = Math.pow(display.value, 2);
-    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить результат в квадрате
+    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить результат в квадрате \ if we need to have the number in degree
         display.value = Math.pow(display.value, 2);
-    } else { //Если предыдущее значение - знак или это посчитанный результат
+    } else { //Если предыдущее значение - знак или это посчитанный результат \ if the last value - it's an operation symbol or a result of Equall function
         return display.value;
     }
 }
 POWER.addEventListener('click', clickPower);
 
 function clickFraction(){
-    if (arrayMemoryNumbers.length < 1 && !changes){ //если экран пуст
+    if (arrayMemoryNumbers.length < 1 && !changes){ //если экран пуст \ if the display is clear
         display.value = 0;
-    } else if (changes) { // Если мы ввели новое число
+    } else if (changes) { // Если мы ввели новое число \ if we write a new number
         display.value = 1 / display.value;
-    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить результат в квадрате
+    } else if (!changes && display.value == resultEquall) { //Если мы хотим получить дробь \ if we need to have fraction
         display.value = 1 / display.value;
-    } else { //Если предыдущее значение - знак или это посчитанный результат
+    } else { //Если предыдущее значение - знак или это посчитанный результат \ if the last value - it's an operation symbol or a result of Equall function
         return display.value;
     }
 }
@@ -120,16 +120,16 @@ function clickBackspase(){
 BACKSPASE.addEventListener('click', clickBackspase);
 
 
-function clickNumber(value){
+function clickNumber(value){ // if we need to write a number
     return function() {
-        if (display.value === '0') { //Если экран пуст
+        if (display.value === '0') { //Если экран пуст \ if the display is clear
             display.value = value;
             changes = true;
         } else {
-            if (changes) { //Если мы вводим число
+            if (changes) { //Если мы вводим число \ if we write a number
                 display.value += value;
                 changes = true;
-            } else { //Если предыдущее значение - математический знак
+            } else { //Если предыдущее значение - математический знак \ if previous value is an operation symbol
                 display.value = value;
                 changes = true;
             }
@@ -147,17 +147,17 @@ SEVEN.addEventListener('click', clickNumber(SEVEN.value));
 EIGHT.addEventListener('click', clickNumber(EIGHT.value));
 NINE.addEventListener('click', clickNumber(NINE.value));
 
-function clickSimpleOperation(value){
+function clickSimpleOperation(value){ //if we need to write an operation symbol
     return function () {
-        if (display.value === 0) { //Если экран полностью пуст
+        if (display.value === 0) { //Если экран полностью пуст \ if the display is clear
             display.value = 0;
         } else {
-            if (changes) { // Если мы ввели новое число
+            if (changes) { // Если мы ввели новое число \ if we write a number
                 arrayMemoryNumbers.push(display.value);
                 displayMemory.value += display.value + value;
                 arrayMemoryOperations.push(value);
                 changes = false;
-            } else if (!changes && display.value == resultEquall) { // Если мы посчитали результат и хотим произвести с ним некоторые операции
+            } else if (!changes && display.value == resultEquall) { // Если мы посчитали результат и хотим произвести с ним некоторые операции // if we need to use trhe result of Equall function
                 arrayMemoryNumbers.push(display.value);
                 displayMemory.value = display.value + value;
                 arrayMemoryOperations.push(value);
@@ -165,9 +165,9 @@ function clickSimpleOperation(value){
             }
             else {
                 let num = arrayMemoryOperations.length - 1;
-                if (arrayMemoryOperations[num] === value && !changes) { //Если мы нажимаем знак повторно
+                if (arrayMemoryOperations[num] === value && !changes) { //Если мы нажимаем знак повторно \ if we press the operation symbol repeatedly 
                     return display.value;
-                } else { // Если мы не вводили цифры и хотим сменить знак
+                } else { // Если мы не вводили цифры и хотим сменить знак \ if we want to change the operation symbol in memory
                     let string = displayMemory.value.substring(0, displayMemory.value.length - 1);
                     displayMemory.value = string + value;
                     arrayMemoryOperations[num] = value;
@@ -182,7 +182,7 @@ MINUS.addEventListener('click', clickSimpleOperation(MINUS.value));
 PLUS.addEventListener('click', clickSimpleOperation(PLUS.value));
 
 
-function clickPlusMinus(){ //Меняем плюс на минус и наоборот перед выражением
+function clickPlusMinus(){ //Меняем плюс на минус и наоборот перед выражением \ changing the +/- symbols before the value
     if (display.value[0] == '-') {
         let displayNow = display.value.substring(1);
         return display.value = displayNow;
@@ -195,7 +195,7 @@ function clickPlusMinus(){ //Меняем плюс на минус и наобо
 }
 PLUSMINUS.addEventListener('click', clickPlusMinus);
 
-function clickDot(){ //Десятичная точка
+function clickDot(){ //Десятичная точка \ Decimal point
     let array = display.value.split('');
     let dotFinder = false;
 
@@ -215,12 +215,12 @@ DOT.addEventListener('click', clickDot);
 
 
 function clickEquall(){
-    //Добавляем в массив последнее введенное значение
+    //Добавляем в массив последнее введенное значение \\ pushing in array the last writing number
     arrayMemoryNumbers.push(display.value);
     displayMemory.value += display.value;
     changes = false;
 
-    //Выполняем действия умножения или деления
+    //Выполняем действия умножения или деления \ performing the '*'|'/' operations
     for (let j = 0; j <= arrayMemoryOperations.length; j++) {
         for (let i = 0; i <= arrayMemoryOperations.length; i++) {
             if (arrayMemoryOperations[i] === '*' ||
@@ -240,7 +240,7 @@ function clickEquall(){
         }
     }
 
-    //Выполняем действия сложения и вычитания
+    //Выполняем действия сложения и вычитания \ performing the '+'|'-' operations
     for (let j = 0; j <= arrayMemoryOperations.length; j++) {
         for (let i = 0; i <= arrayMemoryOperations.length; i++) {
             if (arrayMemoryOperations[i] === '+' ||
